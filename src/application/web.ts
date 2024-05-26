410dc67134ee835dc03435735208dc925af82917
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-05-26 23:08:51 +0700

    feat: create public route and middleware

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,6 +1,16 @@
 import express from "express"
+import { publicRouter } from "../route/public-api"
+import { errorMiddleware } from "../middleware/error-middleware"
 
 export const web = express()
 
 // agar smua body diparse ke json
 web.use(express.json())
+
+// public API
+web.use(publicRouter)
+
+// Authorized API
+
+// Middleware to handle error
+web.use(errorMiddleware)
