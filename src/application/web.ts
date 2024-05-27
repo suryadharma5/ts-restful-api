81a50d1fcf77f9f014cdab2d8b9ee1d2f840f876
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-05-27 22:45:33 +0700

    feat: get current user

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { publicRouter } from "../route/public-api"
 import { errorMiddleware } from "../middleware/error-middleware"
+import { apiRouter } from "../route/api"
 
 export const web = express()
 
@@ -11,6 +12,7 @@ web.use(express.json())
 web.use(publicRouter)
 
 // Authorized API
+web.use(apiRouter)
 
 // Middleware to handle error
 web.use(errorMiddleware)
--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,3 +1,4 @@
+import { User } from "@prisma/client";
 import { prismaClient } from "../application/database";
 import { ResponseError } from "../error/response-error";
 import { CreateUserRequest, LoginUserRequest, UserResponse, toUserResponse } from "../model/user-model";
@@ -64,5 +65,9 @@ export class UserService {
         response.token = user.token!
 
         return response
+    }
+
+    static async get(user: User): Promise<UserResponse> {
+        return toUserResponse(user)
     }
 }
