ea3caa9d0498f889287ccc3a6857b4ed69cc86a1
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-06-03 19:08:50 +0700

    chore: fix file name

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,7 +1,7 @@
 import supertest from "supertest"
 import { web } from "../src/application/web"
 import { logger } from "../src/application/logging"
-import { UserTest } from "./test.-util"
+import { UserTest } from "./test-util"
 import bcrypt from "bcrypt"
 
 describe('POST /api/users', () => {
