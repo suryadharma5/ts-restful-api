398fb5f849cc39b2ab12ae45e10eca761e8a9d64
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-06-18 23:22:34 +0700

    chore: add unit test to get contact api

--- test/contact.test.ts
+++ test/contact.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest"
 import { ContactTest, UserTest } from "./test-util"
 import { web } from "../src/application/web"
 import { logger } from "../src/application/logging"
+import { log } from "winston"
 
 describe('POST /api/contacts', () => {
     beforeEach(async () => {
@@ -77,7 +78,7 @@ describe('GET /api/contacts/:contactId', () => {
         expect(response.body.data.phone).toBe(contact.phone)
     })
 
-    it.only('should not be able to get contact if contact is not found', async () => {
+    it('should not be able to get contact if contact is not found', async () => {
         const contact = await ContactTest.get()
         const response = await supertest(web)
             .get(`/api/contacts/${contact.id + 1}`)
@@ -88,4 +89,56 @@ describe('GET /api/contacts/:contactId', () => {
         expect(response.body.errors).toBeDefined()
     })
 })
+
+describe('/api/contacts/:contactId', () => {
+    beforeEach(async () => {
+        await UserTest.create()
+        await ContactTest.create()
+    })
+
+    afterEach(async () => {
+        await ContactTest.deleteAll()
+        await UserTest.delete()
+    })
+
+    it('should be able to update contact', async () => {
+        const contact = await ContactTest.get()
+        const response = await supertest(web)
+            .put(`/api/contacts/${contact.id}`)
+            .set('X-API-TOKEN', 'test')
+            .send({
+                first_name: "Surya",
+                last_name: "Setiawan",
+                email: "suryas@mail.com",
+                phone: "081136712"
+            })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(200)
+        expect(response.body.data.id).toBe(contact.id)
+        expect(response.body.data.first_name).toBe("Surya")
+        expect(response.body.data.last_name).toBe("Setiawan")
+        expect(response.body.data.email).toBe("suryas@mail.com")
+        expect(response.body.data.phone).toBe("081136712")
+    })
+
+    it('should reject update contact if request is invalid', async () => {
+        const contact = await ContactTest.get()
+        const response = await supertest(web)
+            .put(`/api/contacts/${contact.id}`)
+            .set('X-API-TOKEN', 'test')
+            .send({
+                first_name: "",
+                last_name: "",
+                email: "suryas",
+                phone: ""
+            })
+
+        logger.debug(response.body)
+        expect(response.status).toBe(400)
+        expect(response.body.errors).toBeDefined()
+    })
+
+})
+
 
