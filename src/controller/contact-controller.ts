634a09d220d1b3189919987fc8df676bbf99689d
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-06-25 23:26:29 +0700

    fixed: search request

--- src/controller/contact-controller.ts
+++ src/controller/contact-controller.ts
@@ -62,14 +62,15 @@ export class ContactController {
     static async search(req: UserRequest, res: Response, next: NextFunction) {
         try {
             const request: SearchContactRequest = {
-                name: req.params.name as string,
-                email: req.params.email as string,
-                phone: req.params.phone as string,
+                name: req.query.name as string,
+                email: req.query.email as string,
+                phone: req.query.phone as string,
                 page: req.query.page ? Number(req.query.page) : 1,
                 size: req.query.size ? Number(req.query.size) : 10
             }
             const response = await ContactService.search(req.user!, request)
             logger.debug("response: " + JSON.stringify(response))
+            logger.debug("request: " + JSON.stringify(request))
 
             res.status(200).json(response)
         } catch (e) {
